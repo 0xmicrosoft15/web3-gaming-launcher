Heroic-Games-Launcher/HeroicGamesLauncher

[Tech] Switch from react-scripts to Vite & clean up everything to work with strict mode (#1633)
* Change complete project structure

 - Rename "src" -> "src/frontend"
 - Rename "electron" -> "src/backend"
 - Move types.ts to "common/types.ts"

* Switch from react-scripts + foreman + webpack + ts-loader -> Vite

* Set GH_TOKEN and GITHUB_TOKEN in tests directly

This just cleans up package.json a bit

* Get SVG & JPG importing working in VSCode

* Move type definitions into common/typedefs

* Fixup tests

* Rewrite large chunks of the backend to be compatible with strict mode

* Modify frontend to reflect changes to type definition structure

* Remove unused files

These weren't necessary, at least as far as I can tell

* Update translations

* Fixup: Re-add `--filesystem` parameter for Proton

Turns out refactoring a lot of stuff can introduce issues, who would've
thought

* Remove `build` script

You never have to run this yourself, so it was a bit confusing to have.
If you really need its functionality, `vite build` isn't that hard to
type in on your own :^)

* Fully remove all remnants of UE support

* Add new 'codecheck' command

This command can be used to make sure that no files in the project
violate TS rules, since Vite itself doesn't check them

It would probably be a good idea to add them to GH Workflows and/or
pre-commit hooks, but I'm not too sure on how to do that

* Update translation files

* Fix translation files

Not sure how this happens, but it seems they've gotten screwed up
in the merge

* Remove some more unnecessary files

* Always get game updates from all libraries

This fixes an issue where refreshing the library while only showing
one library made the update icons of all the others disappear

* Some misc code style changes

src/backend/protocol.ts
@@ -1,0 +1,79 @@
+import { BrowserWindow, dialog } from 'electron'
+import { logError, logInfo, LogPrefix } from './logger/logger'
+import i18next from 'i18next'
+import { getGame } from './utils'
+
+export async function handleProtocol(window: BrowserWindow, args: string[]) {
+  // Figure out which argv element is our protocol
+  let url = ''
+  args.forEach((val) => {
+    if (val.startsWith('heroic://')) {
+      url = val
+    }
+  })
+
+  const [scheme, path] = url.split('://')
+  if (!url || scheme !== 'heroic' || !path) {
+    return
+  }
+  let [command, arg] = path.split('/')
+  if (!command || !arg) {
+    command = path
+    arg = ''
+  }
+
+  logInfo(`received '${url}'`, LogPrefix.ProtocolHandler)
+
+  if (command === 'ping') {
+    return logInfo(['Received ping! Arg:', arg], LogPrefix.ProtocolHandler)
+  }
+
+  if (command === 'launch') {
+    const game =
+      getGame(arg, 'legendary').getGameInfo() ||
+      getGame(arg, 'gog').getGameInfo()
+
+    if (!game) {
+      return logError(
+        `Could not receive game data for ${arg}!`,
+        LogPrefix.ProtocolHandler
+      )
+    }
+
+    const { is_installed, title, app_name, runner } = game
+    if (!is_installed) {
+      logInfo(`"${arg}" not installed.`, LogPrefix.ProtocolHandler)
+      const { response } = await dialog.showMessageBox(window, {
+        buttons: [i18next.t('box.yes'), i18next.t('box.no')],
+        cancelId: 1,
+        message: `${title} ${i18next.t(
+          'box.protocol.install.not_installed',
+          'Is Not Installed, do you wish to Install it?'
+        )}`,
+        title: title
+      })
+      if (response === 0) {
+        const { filePaths, canceled } = await dialog.showOpenDialog({
+          buttonLabel: i18next.t('box.choose'),
+          properties: ['openDirectory'],
+          title: i18next.t('install.path', 'Select Install Path')
+        })
+        if (canceled) {
+          return
+        }
+        if (filePaths[0]) {
+          return window.webContents.send('installGame', {
+            appName: app_name,
+            runner,
+            installPath: filePaths[0]
+          })
+        }
+      }
+      if (response === 1) {
+        return logInfo('Not installing game', LogPrefix.ProtocolHandler)
+      }
+    }
+    window.hide()
+    window.webContents.send('launchGame', arg, runner)
+  }
+}

src/frontend/screens/Settings/components/GeneralSettings/index.tsx
@@ -1,0 +1,356 @@
+import React, { useContext, useEffect, useState } from 'react'
+
+import { LibraryTopSectionOptions, Path } from 'frontend/types'
+import { useTranslation } from 'react-i18next'
+import ContextProvider from 'frontend/state/ContextProvider'
+import { InfoBox, SelectField, ToggleSwitch } from 'frontend/components/UI'
+import LanguageSelector from 'frontend/components/UI/LanguageSelector'
+
+import Backspace from '@mui/icons-material/Backspace'
+import { toggleControllerIsDisabled } from 'frontend/helpers/gamepad'
+import TextInputWithIconField from 'frontend/components/UI/TextInputWithIconField'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+
+import { ipcRenderer } from 'frontend/helpers'
+interface Props {
+  darkTrayIcon: boolean
+  defaultInstallPath: string
+  disableController: boolean
+  egsLinkedPath: string
+  egsPath: string
+  exitToTray: boolean
+  maxWorkers: number
+  minimizeOnLaunch: boolean
+  checkForUpdatesOnStartup: boolean
+  setDefaultInstallPath: (value: string) => void
+  setEgsLinkedPath: (value: string) => void
+  setEgsPath: (value: string) => void
+  setMaxWorkers: (value: number) => void
+  startInTray: boolean
+  toggleDarkTrayIcon: () => void
+  toggleDisableController: () => void
+  toggleStartInTray: () => void
+  toggleTray: () => void
+  toggleMinimizeOnLaunch: () => void
+  toggleUpdatesOnStartup: () => void
+}
+
+export default function GeneralSettings({
+  defaultInstallPath,
+  setDefaultInstallPath,
+  egsPath,
+  setEgsPath,
+  egsLinkedPath,
+  setEgsLinkedPath,
+  exitToTray,
+  startInTray,
+  toggleTray,
+  toggleStartInTray,
+  maxWorkers,
+  setMaxWorkers,
+  darkTrayIcon,
+  toggleDarkTrayIcon,
+  minimizeOnLaunch,
+  toggleMinimizeOnLaunch,
+  disableController,
+  toggleDisableController,
+  checkForUpdatesOnStartup,
+  toggleUpdatesOnStartup
+}: Props) {
+  const [showUpdateSettings, setShowUpdateSettings] = useState(
+    checkForUpdatesOnStartup
+  )
+  const [isSyncing, setIsSyncing] = useState(false)
+  const [maxCpus, setMaxCpus] = useState(maxWorkers)
+  const {
+    platform,
+    refreshLibrary,
+    libraryTopSection,
+    handleLibraryTopSection
+  } = useContext(ContextProvider)
+  const { t } = useTranslation()
+  const isLinked = Boolean(egsLinkedPath.length)
+  const isWindows = platform === 'win32'
+
+  useEffect(() => {
+    const getMoreInfo = async () => {
+      const cores = await ipcRenderer.invoke('getMaxCpus')
+      setMaxCpus(cores)
+    }
+    getMoreInfo()
+  }, [maxWorkers])
+
+  useEffect(() => {
+    ipcRenderer
+      .invoke('showUpdateSetting')
+      .then((s) => setShowUpdateSettings(s))
+  }, [])
+
+  async function handleSync() {
+    setIsSyncing(true)
+    if (isLinked) {
+      return ipcRenderer.invoke('egsSync', 'unlink').then(async () => {
+        await ipcRenderer.invoke('openMessageBox', {
+          message: t('message.unsync'),
+          title: 'EGS Sync'
+        })
+        setEgsLinkedPath('')
+        setEgsPath('')
+        setIsSyncing(false)
+        refreshLibrary({ fullRefresh: true, runInBackground: false })
+      })
+    }
+
+    return ipcRenderer.invoke('egsSync', egsPath).then(async (res: string) => {
+      if (res === 'Error') {
+        setIsSyncing(false)
+        ipcRenderer.invoke('showErrorBox', [
+          t('box.error.title', 'Error'),
+          t('box.sync.error')
+        ])
+        setEgsLinkedPath('')
+        setEgsPath('')
+        return
+      }
+      await ipcRenderer.invoke('openMessageBox', {
+        message: t('message.sync'),
+        title: 'EGS Sync'
+      })
+
+      setIsSyncing(false)
+      setEgsLinkedPath(isWindows ? 'windows' : egsPath)
+      refreshLibrary({ fullRefresh: true, runInBackground: false })
+    })
+  }
+
+  function handleEgsFolder() {
+    if (isLinked) {
+      return ''
+    }
+    return ipcRenderer
+      .invoke('openDialog', {
+        buttonLabel: t('box.choose'),
+        properties: ['openDirectory'],
+        title: t('box.choose-egs-prefix')
+      })
+      .then(({ path }: Path) => setEgsPath(path ? path : ''))
+  }
+
+  return (
+    <>
+      <h3 className="settingSubheader">{t('settings.navbar.general')}</h3>
+
+      <LanguageSelector />
+
+      <TextInputWithIconField
+        label={t('setting.default-install-path')}
+        htmlId="default_install_path"
+        value={defaultInstallPath?.replaceAll("'", '')}
+        placeholder={defaultInstallPath}
+        onChange={(event) => setDefaultInstallPath(event.target.value)}
+        icon={
+          <FontAwesomeIcon
+            icon={faFolderOpen}
+            data-testid="setinstallpathbutton"
+          />
+        }
+        onIconClick={async () =>
+          ipcRenderer
+            .invoke('openDialog', {
+              buttonLabel: t('box.choose'),
+              properties: ['openDirectory'],
+              title: t('box.default-install-path'),
+              defaultPath: defaultInstallPath
+            })
+            .then(({ path }: Path) =>
+              setDefaultInstallPath(path ? `${path}` : defaultInstallPath)
+            )
+        }
+      />
+
+      {!isWindows && (
+        <TextInputWithIconField
+          label={t('setting.egs-sync')}
+          extraClass="withRightButton"
+          htmlId="set_epic_sync_path"
+          placeholder={t('placeholder.egs-prefix')}
+          value={egsPath || egsLinkedPath}
+          disabled={isLinked}
+          onChange={(event) => setEgsPath(event.target.value)}
+          icon={
+            !egsPath.length ? (
+              <FontAwesomeIcon
+                icon={faFolderOpen}
+                data-testid="setEpicSyncPathButton"
+                style={{
+                  color: isLinked ? 'transparent' : 'currentColor'
+                }}
+              />
+            ) : (
+              <Backspace
+                data-testid="setEpicSyncPathBackspace"
+                style={
+                  isLinked
+                    ? { color: 'transparent', pointerEvents: 'none' }
+                    : { color: '#B0ABB6' }
+                }
+              />
+            )
+          }
+          onIconClick={
+            !egsPath.length
+              ? () => handleEgsFolder()
+              : () => (isLinked ? '' : setEgsPath(''))
+          }
+          afterInput={
+            <>
+              <span className="rightButton">
+                <button
+                  data-testid="syncButton"
+                  onClick={async () => handleSync()}
+                  disabled={isSyncing || !egsPath.length}
+                  className={`button is-small ${
+                    isLinked
+                      ? 'is-danger'
+                      : isSyncing
+                      ? 'is-primary'
+                      : 'settings'
+                  }`}
+                >
+                  {`${
+                    isLinked
+                      ? t('button.unsync')
+                      : isSyncing
+                      ? t('button.syncing')
+                      : t('button.sync')
+                  }`}
+                </button>
+              </span>
+              <div>
+                {!isWindows && (
+                  <InfoBox text="infobox.help">{t('help.general')}</InfoBox>
+                )}
+              </div>
+            </>
+          }
+        />
+      )}
+
+      {isWindows && (
+        <ToggleSwitch
+          htmlId="syncToggle"
+          value={isLinked}
+          handleChange={handleSync}
+          title={t('setting.egs-sync')}
+        />
+      )}
+
+      {showUpdateSettings && (
+        <ToggleSwitch
+          htmlId="checkForUpdatesOnStartup"
+          value={checkForUpdatesOnStartup}
+          handleChange={toggleUpdatesOnStartup}
+          title={t(
+            'setting.checkForUpdatesOnStartup',
+            'Check for Heroic Updates on Startup'
+          )}
+        />
+      )}
+
+      <ToggleSwitch
+        htmlId="exitToTray"
+        value={exitToTray}
+        handleChange={toggleTray}
+        title={t('setting.exit-to-tray')}
+      />
+
+      {exitToTray && (
+        <ToggleSwitch
+          htmlId="startInTray"
+          value={startInTray}
+          handleChange={toggleStartInTray}
+          title={t('setting.start-in-tray', 'Start Minimized')}
+        />
+      )}
+
+      <ToggleSwitch
+        htmlId="minimizeOnLaunch"
+        value={minimizeOnLaunch}
+        handleChange={toggleMinimizeOnLaunch}
+        title={t(
+          'setting.minimize-on-launch',
+          'Minimize Heroic After Game Launch'
+        )}
+      />
+
+      <ToggleSwitch
+        htmlId="changeTrayColor"
+        value={darkTrayIcon}
+        handleChange={() => {
+          toggleDarkTrayIcon()
+          return ipcRenderer.send('changeTrayColor')
+        }}
+        title={t('setting.darktray', 'Use Dark Tray Icon (needs restart)')}
+      />
+
+      <ToggleSwitch
+        htmlId="disableController"
+        value={disableController}
+        handleChange={() => {
+          toggleDisableController()
+          toggleControllerIsDisabled(!disableController)
+        }}
+        title={t(
+          'setting.disable_controller',
+          'Disable Heroic navigation using controller'
+        )}
+      />
+
+      <SelectField
+        label={t('setting.library_top_section', 'Library Top Section')}
+        htmlId="library_top_section_selector"
+        onChange={(event) =>
+          handleLibraryTopSection(
+            event.target.value as LibraryTopSectionOptions
+          )
+        }
+        value={libraryTopSection}
+      >
+        <option value="recently_played">
+          {t(
+            'setting.library_top_option.recently_played',
+            'Recently Played Games'
+          )}
+        </option>
+        <option value="recently_played_installed">
+          {t(
+            'setting.library_top_option.recently_played_installed',
+            'Recently Played Games (Only Installed)'
+          )}
+        </option>
+        <option value="favourites">
+          {t('setting.library_top_option.favourites', 'Favourite Games')}
+        </option>
+        <option value="disabled">
+          {t('setting.library_top_option.disabled', 'Disabled')}
+        </option>
+      </SelectField>
+
+      <SelectField
+        htmlId="max_workers"
+        label={t('setting.maxworkers')}
+        onChange={(event) => setMaxWorkers(Number(event.target.value))}
+        value={maxWorkers.toString()}
+        extraClass="smaller"
+      >
+        {Array.from(Array(maxCpus).keys()).map((n) => (
+          <option key={n + 1}>{n + 1}</option>
+        ))}
+        <option key={0} value={0}>
+          Max
+        </option>
+      </SelectField>
+    </>
+  )
+}

0xmicrosoft15/web3-gaming-launcher

src/backend/protocol.ts
@@ -1,0 +1,79 @@
+import { BrowserWindow, dialog } from 'electron'
+import { logError, logInfo, LogPrefix } from './logger/logger'
+import i18next from 'i18next'
+import { getGame } from './utils'
+
+export async function handleProtocol(window: BrowserWindow, args: string[]) {
+  // Figure out which argv element is our protocol
+  let url = ''
+  args.forEach((val) => {
+    if (val.startsWith('hyperplay://')) {
+      url = val
+    }
+  })
+
+  const [scheme, path] = url.split('://')
+  if (!url || scheme !== 'hyperplay' || !path) {
+    return
+  }
+  let [command, arg] = path.split('/')
+  if (!command || !arg) {
+    command = path
+    arg = ''
+  }
+
+  logInfo(`received '${url}'`, LogPrefix.ProtocolHandler)
+
+  if (command === 'ping') {
+    return logInfo(['Received ping! Arg:', arg], LogPrefix.ProtocolHandler)
+  }
+
+  if (command === 'launch') {
+    const game =
+      getGame(arg, 'legendary').getGameInfo() ||
+      getGame(arg, 'gog').getGameInfo()
+
+    if (!game) {
+      return logError(
+        `Could not receive game data for ${arg}!`,
+        LogPrefix.ProtocolHandler
+      )
+    }
+
+    const { is_installed, title, app_name, runner } = game
+    if (!is_installed) {
+      logInfo(`"${arg}" not installed.`, LogPrefix.ProtocolHandler)
+      const { response } = await dialog.showMessageBox(window, {
+        buttons: [i18next.t('box.yes'), i18next.t('box.no')],
+        cancelId: 1,
+        message: `${title} ${i18next.t(
+          'box.protocol.install.not_installed',
+          'Is Not Installed, do you wish to Install it?'
+        )}`,
+        title: title
+      })
+      if (response === 0) {
+        const { filePaths, canceled } = await dialog.showOpenDialog({
+          buttonLabel: i18next.t('box.choose'),
+          properties: ['openDirectory'],
+          title: i18next.t('install.path', 'Select Install Path')
+        })
+        if (canceled) {
+          return
+        }
+        if (filePaths[0]) {
+          return window.webContents.send('installGame', {
+            appName: app_name,
+            runner,
+            installPath: filePaths[0]
+          })
+        }
+      }
+      if (response === 1) {
+        return logInfo('Not installing game', LogPrefix.ProtocolHandler)
+      }
+    }
+    window.hide()
+    window.webContents.send('launchGame', arg, runner)
+  }
+}

src/frontend/screens/Settings/components/GeneralSettings/index.tsx
@@ -1,0 +1,356 @@
+import React, { useContext, useEffect, useState } from 'react'
+
+import { LibraryTopSectionOptions, Path } from 'frontend/types'
+import { useTranslation } from 'react-i18next'
+import ContextProvider from 'frontend/state/ContextProvider'
+import { InfoBox, SelectField, ToggleSwitch } from 'frontend/components/UI'
+import LanguageSelector from 'frontend/components/UI/LanguageSelector'
+
+import Backspace from '@mui/icons-material/Backspace'
+import { toggleControllerIsDisabled } from 'frontend/helpers/gamepad'
+import TextInputWithIconField from 'frontend/components/UI/TextInputWithIconField'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+
+import { ipcRenderer } from 'frontend/helpers'
+interface Props {
+  darkTrayIcon: boolean
+  defaultInstallPath: string
+  disableController: boolean
+  egsLinkedPath: string
+  egsPath: string
+  exitToTray: boolean
+  maxWorkers: number
+  minimizeOnLaunch: boolean
+  checkForUpdatesOnStartup: boolean
+  setDefaultInstallPath: (value: string) => void
+  setEgsLinkedPath: (value: string) => void
+  setEgsPath: (value: string) => void
+  setMaxWorkers: (value: number) => void
+  startInTray: boolean
+  toggleDarkTrayIcon: () => void
+  toggleDisableController: () => void
+  toggleStartInTray: () => void
+  toggleTray: () => void
+  toggleMinimizeOnLaunch: () => void
+  toggleUpdatesOnStartup: () => void
+}
+
+export default function GeneralSettings({
+  defaultInstallPath,
+  setDefaultInstallPath,
+  egsPath,
+  setEgsPath,
+  egsLinkedPath,
+  setEgsLinkedPath,
+  exitToTray,
+  startInTray,
+  toggleTray,
+  toggleStartInTray,
+  maxWorkers,
+  setMaxWorkers,
+  darkTrayIcon,
+  toggleDarkTrayIcon,
+  minimizeOnLaunch,
+  toggleMinimizeOnLaunch,
+  disableController,
+  toggleDisableController,
+  checkForUpdatesOnStartup,
+  toggleUpdatesOnStartup
+}: Props) {
+  const [showUpdateSettings, setShowUpdateSettings] = useState(
+    checkForUpdatesOnStartup
+  )
+  const [isSyncing, setIsSyncing] = useState(false)
+  const [maxCpus, setMaxCpus] = useState(maxWorkers)
+  const {
+    platform,
+    refreshLibrary,
+    libraryTopSection,
+    handleLibraryTopSection
+  } = useContext(ContextProvider)
+  const { t } = useTranslation()
+  const isLinked = Boolean(egsLinkedPath.length)
+  const isWindows = platform === 'win32'
+
+  useEffect(() => {
+    const getMoreInfo = async () => {
+      const cores = await ipcRenderer.invoke('getMaxCpus')
+      setMaxCpus(cores)
+    }
+    getMoreInfo()
+  }, [maxWorkers])
+
+  useEffect(() => {
+    ipcRenderer
+      .invoke('showUpdateSetting')
+      .then((s) => setShowUpdateSettings(s))
+  }, [])
+
+  async function handleSync() {
+    setIsSyncing(true)
+    if (isLinked) {
+      return ipcRenderer.invoke('egsSync', 'unlink').then(async () => {
+        await ipcRenderer.invoke('openMessageBox', {
+          message: t('message.unsync'),
+          title: 'EGS Sync'
+        })
+        setEgsLinkedPath('')
+        setEgsPath('')
+        setIsSyncing(false)
+        refreshLibrary({ fullRefresh: true, runInBackground: false })
+      })
+    }
+
+    return ipcRenderer.invoke('egsSync', egsPath).then(async (res: string) => {
+      if (res === 'Error') {
+        setIsSyncing(false)
+        ipcRenderer.invoke('showErrorBox', [
+          t('box.error.title', 'Error'),
+          t('box.sync.error')
+        ])
+        setEgsLinkedPath('')
+        setEgsPath('')
+        return
+      }
+      await ipcRenderer.invoke('openMessageBox', {
+        message: t('message.sync'),
+        title: 'EGS Sync'
+      })
+
+      setIsSyncing(false)
+      setEgsLinkedPath(isWindows ? 'windows' : egsPath)
+      refreshLibrary({ fullRefresh: true, runInBackground: false })
+    })
+  }
+
+  function handleEgsFolder() {
+    if (isLinked) {
+      return ''
+    }
+    return ipcRenderer
+      .invoke('openDialog', {
+        buttonLabel: t('box.choose'),
+        properties: ['openDirectory'],
+        title: t('box.choose-egs-prefix')
+      })
+      .then(({ path }: Path) => setEgsPath(path ? path : ''))
+  }
+
+  return (
+    <>
+      <h3 className="settingSubheader">{t('settings.navbar.general')}</h3>
+
+      <LanguageSelector />
+
+      <TextInputWithIconField
+        label={t('setting.default-install-path')}
+        htmlId="default_install_path"
+        value={defaultInstallPath?.replaceAll("'", '')}
+        placeholder={defaultInstallPath}
+        onChange={(event) => setDefaultInstallPath(event.target.value)}
+        icon={
+          <FontAwesomeIcon
+            icon={faFolderOpen}
+            data-testid="setinstallpathbutton"
+          />
+        }
+        onIconClick={async () =>
+          ipcRenderer
+            .invoke('openDialog', {
+              buttonLabel: t('box.choose'),
+              properties: ['openDirectory'],
+              title: t('box.default-install-path'),
+              defaultPath: defaultInstallPath
+            })
+            .then(({ path }: Path) =>
+              setDefaultInstallPath(path ? `${path}` : defaultInstallPath)
+            )
+        }
+      />
+
+      {!isWindows && (
+        <TextInputWithIconField
+          label={t('setting.egs-sync')}
+          extraClass="withRightButton"
+          htmlId="set_epic_sync_path"
+          placeholder={t('placeholder.egs-prefix')}
+          value={egsPath || egsLinkedPath}
+          disabled={isLinked}
+          onChange={(event) => setEgsPath(event.target.value)}
+          icon={
+            !egsPath.length ? (
+              <FontAwesomeIcon
+                icon={faFolderOpen}
+                data-testid="setEpicSyncPathButton"
+                style={{
+                  color: isLinked ? 'transparent' : 'currentColor'
+                }}
+              />
+            ) : (
+              <Backspace
+                data-testid="setEpicSyncPathBackspace"
+                style={
+                  isLinked
+                    ? { color: 'transparent', pointerEvents: 'none' }
+                    : { color: '#B0ABB6' }
+                }
+              />
+            )
+          }
+          onIconClick={
+            !egsPath.length
+              ? () => handleEgsFolder()
+              : () => (isLinked ? '' : setEgsPath(''))
+          }
+          afterInput={
+            <>
+              <span className="rightButton">
+                <button
+                  data-testid="syncButton"
+                  onClick={async () => handleSync()}
+                  disabled={isSyncing || !egsPath.length}
+                  className={`button is-small ${
+                    isLinked
+                      ? 'is-danger'
+                      : isSyncing
+                      ? 'is-primary'
+                      : 'settings'
+                  }`}
+                >
+                  {`${
+                    isLinked
+                      ? t('button.unsync')
+                      : isSyncing
+                      ? t('button.syncing')
+                      : t('button.sync')
+                  }`}
+                </button>
+              </span>
+              <div>
+                {!isWindows && (
+                  <InfoBox text="infobox.help">{t('help.general')}</InfoBox>
+                )}
+              </div>
+            </>
+          }
+        />
+      )}
+
+      {isWindows && (
+        <ToggleSwitch
+          htmlId="syncToggle"
+          value={isLinked}
+          handleChange={handleSync}
+          title={t('setting.egs-sync')}
+        />
+      )}
+
+      {showUpdateSettings && (
+        <ToggleSwitch
+          htmlId="checkForUpdatesOnStartup"
+          value={checkForUpdatesOnStartup}
+          handleChange={toggleUpdatesOnStartup}
+          title={t(
+            'setting.checkForUpdatesOnStartup',
+            'Check for HyperPlay Updates on Startup'
+          )}
+        />
+      )}
+
+      <ToggleSwitch
+        htmlId="exitToTray"
+        value={exitToTray}
+        handleChange={toggleTray}
+        title={t('setting.exit-to-tray')}
+      />
+
+      {exitToTray && (
+        <ToggleSwitch
+          htmlId="startInTray"
+          value={startInTray}
+          handleChange={toggleStartInTray}
+          title={t('setting.start-in-tray', 'Start Minimized')}
+        />
+      )}
+
+      <ToggleSwitch
+        htmlId="minimizeOnLaunch"
+        value={minimizeOnLaunch}
+        handleChange={toggleMinimizeOnLaunch}
+        title={t(
+          'setting.minimize-on-launch',
+          'Minimize HyperPlay After Game Launch'
+        )}
+      />
+
+      <ToggleSwitch
+        htmlId="changeTrayColor"
+        value={darkTrayIcon}
+        handleChange={() => {
+          toggleDarkTrayIcon()
+          return ipcRenderer.send('changeTrayColor')
+        }}
+        title={t('setting.darktray', 'Use Dark Tray Icon (needs restart)')}
+      />
+
+      <ToggleSwitch
+        htmlId="disableController"
+        value={disableController}
+        handleChange={() => {
+          toggleDisableController()
+          toggleControllerIsDisabled(!disableController)
+        }}
+        title={t(
+          'setting.disable_controller',
+          'Disable HyperPlay navigation using controller'
+        )}
+      />
+
+      <SelectField
+        label={t('setting.library_top_section', 'Library Top Section')}
+        htmlId="library_top_section_selector"
+        onChange={(event) =>
+          handleLibraryTopSection(
+            event.target.value as LibraryTopSectionOptions
+          )
+        }
+        value={libraryTopSection}
+      >
+        <option value="recently_played">
+          {t(
+            'setting.library_top_option.recently_played',
+            'Recently Played Games'
+          )}
+        </option>
+        <option value="recently_played_installed">
+          {t(
+            'setting.library_top_option.recently_played_installed',
+            'Recently Played Games (Only Installed)'
+          )}
+        </option>
+        <option value="favourites">
+          {t('setting.library_top_option.favourites', 'Favourite Games')}
+        </option>
+        <option value="disabled">
+          {t('setting.library_top_option.disabled', 'Disabled')}
+        </option>
+      </SelectField>
+
+      <SelectField
+        htmlId="max_workers"
+        label={t('setting.maxworkers')}
+        onChange={(event) => setMaxWorkers(Number(event.target.value))}
+        value={maxWorkers.toString()}
+        extraClass="smaller"
+      >
+        {Array.from(Array(maxCpus).keys()).map((n) => (
+          <option key={n + 1}>{n + 1}</option>
+        ))}
+        <option key={0} value={0}>
+          Max
+        </option>
+      </SelectField>
+    </>
+  )
+}